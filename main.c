86b4028faee0c0a326514670421641021d1e78af
Felipe Quaresma Vieira <135285820+felipeqvieira@users.noreply.github.com>
2023-10-05 11:05:53 -0300

    Update main.c
    Criei um protótipo de main

--- main.c
+++ main.c
@@ -8,5 +8,35 @@
 
 int main(void)
 {
+
+  int n, k, i;
+  scanf("%d", &n);
+  scanf("%d", &k);
+
+  double x[k], y[k];
+
+  for(i = 0; i < k; i++){
+    scanf("%lf %lf", &x[i], &y[i]);
+  }
+
+  double coefficients[n+1];
+
+  ajusteMinimosQuadrados(k, x, y, n, coefficients);
+
+
+  for(i = 0; i <= n; i++){
+    printf("%.10lf\n", coefficients[i]);
+  }
+
+  double res[k];
+
+  for(i = 0; i < k; i++){
+    res[i] = y[i] - f(x[i], coefficients, n);
+  }
+
+  for(i = 0; i < k; i++){
+    printf("%.10lf\n", res[i]);
+  }
+
   return 0;
 }
