698714234cc660ef188c95638871b2ba1ca29194
Felipe Quaresma Vieira <135285820+felipeqvieira@users.noreply.github.com>
2023-10-06 17:34:31 -0300

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -1,15 +1,17 @@
-#include "float_operations.h"
+#include "float_ops.h"
 #include <math.h>
 #include <float.h>
 #include <stdlib.h>
 #include <stdint.h>
 
+/* Estrutura da matriz */
 typedef struct
 {
   size_t size;
   Interval **data;
 } Matrix;
 
+/* Estrutura do Vetor */
 typedef struct
 {
   size_t size;
@@ -17,3 +19,5 @@ typedef struct
 } Vector;
 
 void gaussEliminationWithPivoting(Matrix *m, Vector *c);
+Vector *printSolutionBySubstitution(Matrix m, Vector c);
+int testeGauss(void);
