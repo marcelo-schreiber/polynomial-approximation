139e6a445e8393ef89f83c68cd4338dde1445cd5
Felipe Quaresma Vieira <135285820+felipeqvieira@users.noreply.github.com>
2023-10-06 17:36:59 -0300

    Update mmq.h

--- mmq.h
+++ mmq.h
@@ -1,1 +1,17 @@
-void ajusteMinimosQuadrados(int n, double x[], double y[], int degree, double coefficients[]);
+#include <stdlib.h>
+
+/* Estrutura de um ponto */
+typedef struct point
+{
+    double x;
+    double y;
+} point_t;
+
+/* Esttura de pontos */
+typedef struct points
+{
+    point_t *points;
+    int num_of_points;
+} points_t;
+
+void aproximacao_polinomial_usando_mmq(points_t *points, unsigned int order);
