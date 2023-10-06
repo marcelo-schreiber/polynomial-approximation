dc826d8a6bad39ec54d66c7307d79a3649e134de
Felipe Quaresma Vieira <135285820+felipeqvieira@users.noreply.github.com>
2023-10-06 17:34:00 -0300

    Update main.c

--- main.c
+++ main.c
@@ -6,37 +6,18 @@
  * Data: 8/10/2023
  */
 
+#include "mmq.h"
+#include <stdio.h>
+
 int main(void)
 {
 
   int n, k, i;
-  scanf("%d", &n);
-  scanf("%d", &k);
+  read_n_k(&n, &k);
 
-  double x[k], y[k];
+  points_t *points = read_points(n);
 
-  for(i = 0; i < k; i++){
-    scanf("%lf %lf", &x[i], &y[i]);
-  }
-
-  double coefficients[n+1];
-
-  ajusteMinimosQuadrados(k, x, y, n, coefficients);
-
-
-  for(i = 0; i <= n; i++){
-    printf("%.10lf\n", coefficients[i]);
-  }
-
-  double res[k];
-
-  for(i = 0; i < k; i++){
-    res[i] = y[i] - f(x[i], coefficients, n);
-  }
-
-  for(i = 0; i < k; i++){
-    printf("%.10lf\n", res[i]);
-  }
+  aproximate_function_by_points_using_least_square(points, n - 1);
 
   return 0;
 }
