dc05c5b038aa365b79cacdce4d0c57c580332cb8
Felipe Quaresma Vieira <135285820+felipeqvieira@users.noreply.github.com>
2023-10-06 17:33:46 -0300

    Update auxiliares.h

--- auxiliares.h
+++ auxiliares.h
@@ -1,3 +1,4 @@
+void read_n_k(int *n, int *k);
 points_t *read_points(int n);
 Matrix cria_matrix_A(points_t *points, unsigned int order);
 Vector cria_vector_B(points_t *points, unsigned int order);
