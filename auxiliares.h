27b26b440a49bcf9871edf4191c81989901e1a7a
Felipe Quaresma Vieira <135285820+felipeqvieira@users.noreply.github.com>
2023-10-06 17:27:59 -0300

    Create auxiliares.h

---
+++ auxiliares.h
@@ -0,0 +1,5 @@
+points_t *read_points(int n);
+Matrix cria_matrix_A(points_t *points, unsigned int order);
+Vector cria_vector_B(points_t *points, unsigned int order);
+void print_coeficientes(Vector *b);
+void print_residuo(points_t *points, Vector *b);
