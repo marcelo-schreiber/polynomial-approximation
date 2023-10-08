934425edfa1eb2a33fdf5415f1f7c0158b05bb73
Felipe Quaresma Vieira <135285820+felipeqvieira@users.noreply.github.com>
2023-10-08 18:03:57 -0300

    Update interval_operations.h

--- interval_operations.h
+++ interval_operations.h
@@ -4,28 +4,42 @@
 #include <fenv.h>
 #include <stdint.h>
 
+/* Estrutura que auxilia na conversão double->float */
 typedef union
 {
   double f;
   uint64_t u;
 } DoubleIntUnion;
 
+/* estrutura do intervalo de um double */
 typedef struct
 {
   double lower;
   double upper;
 } Interval;
 
+/* função da conversão double->float */
 uint64_t convert_double_to_int64(double dbl);
 
+/* intervalo inferior */
 double m(double x);
+/* intervalo superior */
 double M(double x);
 
+/* função que cria o intervalo de um double */
 Interval interval(double x);
 
+/* operação de soma de intervalos */
 Interval interval_sum(Interval a, Interval b);
+
+/* operação de subtração de intervalos */
 Interval interval_sub(Interval a, Interval b);
+
+/* operação de multiplicação */
 Interval interval_mul(Interval a, Interval b);
+
+/* operação de divisão de intervalos */
 Interval interval_div(Interval a, Interval b);
-Interval apply_op(Interval a, Interval b, char op);
+
+/* operação de potenciação de um intervalo */
 Interval interval_pow(Interval i, int p);
